6c3d30d033bb4fb71ffa46d7eb50a2f1b915da64
Vaibhav Sathe <vaibhavsathe160@gmail.com>
2025-02-14 09:27:26 +0530

    Starts with user authentication

--- Backend/server.js
+++ Backend/server.js
@@ -1,11 +1,10 @@
 import "dotenv/config";
-
 import app from "./app.js";
-import http from 'http';
+import http from "http";
 const PORT = process.env.PORT || 8899;
 
 const server = http.createServer(app);
 
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
-})
+});
