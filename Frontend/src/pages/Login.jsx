a40a4108e35153c312c4e756b756500bab59d339
Vaibhav Sathe <vaibhavsathe160@gmail.com>
2025-02-17 18:35:21 +0530

    Login page ui done

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -1,5 +1,6 @@
 // import React from "react";
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
   const [hidePass, setHidePass] = useState("password");
@@ -21,36 +22,45 @@ const Login = () => {
         <h2 className="font-bold text-2xl text-white mb-4">Login</h2>
         <form>
           <div className="mb-3">
+            <label className="block text-gray-400 mb-2" htmlFor="email">
+              Email
+            </label>
             <input
               type="email"
+              id="email"
               className="w-full bg-gray-700 p-3 outline-none rounded-lg text-white border-none"
               placeholder="Email"
             />
           </div>
           <div className="mb-3 relative">
+            <label className="block text-gray-400 mb-2" htmlFor="password">
+              Password
+            </label>
             <h2
               onClick={() => handleShowPass()}
-              className="text-xl absolute right-5 top-3 cursor-pointer"
+              className="text-xl absolute right-5 top-10 cursor-pointer"
             >
               <i
-                className={`${eyeChange ? "ri-eye-line" : "ri-eye-off-line"}`}
+                className={`${eyeChange ? "ri-eye-off-line" : "ri-eye-line"}`}
               ></i>
             </h2>
             <input
               type={hidePass}
+              id="password"
               className="w-full bg-gray-700 p-3 outline-none rounded-lg text-white border-none"
               placeholder="Password"
             />
           </div>
           <button
-            className="w-full bg-gray-700 p-3 outline-none rounded-lg text-white border-none"
+            className="w-full bg-blue-500 hover:bg-blue-600 mt-2 p-3 outline-none rounded-lg text-white border-none"
             type="submit"
           >
             Login {/* Fixed button text */}
           </button>
         </form>
-        <p className="text-center text-white mt-3">
-          Don&apos;t have account? <a>Register</a>
+        <p className="text-center text-gray-400 mt-3">
+          Don&apos;t have account?{" "}
+          <Link className="text-blue-500 hover:underline">Register</Link>
         </p>
       </div>
     </div>
