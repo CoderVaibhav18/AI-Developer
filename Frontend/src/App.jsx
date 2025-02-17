161ac9398aaf6d5aa8854c29cac0e6bbe07ce6d0
Vaibhav Sathe <vaibhavsathe160@gmail.com>
2025-02-17 18:24:41 +0530

    Login Page done

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 // import Home from "./pages/Home";
 import AppRoutes from "./Routes/AppRoutes";
+import 'remixicon/fonts/remixicon.css'
 
 function App() {
   return (
--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -1,7 +1,60 @@
-import React from "react";
+// import React from "react";
+import { useState } from "react";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [hidePass, setHidePass] = useState("password");
+  const [eyeChange, setEyeChange] = useState(true); // Fixed initial state
+
+  const handleShowPass = () => {
+    if (eyeChange) {
+      setEyeChange(false);
+      setHidePass("text");
+    } else {
+      setEyeChange(true);
+      setHidePass("password");
+    }
+  };
+
+  return (
+    <div className="h-screen flex items-center justify-center bg-gray-900">
+      <div className="bg-gray-800 w-full max-w-md p-8 rounded-lg">
+        <h2 className="font-bold text-2xl text-white mb-4">Login</h2>
+        <form>
+          <div className="mb-3">
+            <input
+              type="email"
+              className="w-full bg-gray-700 p-3 outline-none rounded-lg text-white border-none"
+              placeholder="Email"
+            />
+          </div>
+          <div className="mb-3 relative">
+            <h2
+              onClick={() => handleShowPass()}
+              className="text-xl absolute right-5 top-3 cursor-pointer"
+            >
+              <i
+                className={`${eyeChange ? "ri-eye-line" : "ri-eye-off-line"}`}
+              ></i>
+            </h2>
+            <input
+              type={hidePass}
+              className="w-full bg-gray-700 p-3 outline-none rounded-lg text-white border-none"
+              placeholder="Password"
+            />
+          </div>
+          <button
+            className="w-full bg-gray-700 p-3 outline-none rounded-lg text-white border-none"
+            type="submit"
+          >
+            Login {/* Fixed button text */}
+          </button>
+        </form>
+        <p className="text-center text-white mt-3">
+          Don&apos;t have account? <a>Register</a>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
