02bc5aa4d1b6328aa8d76e458a060ed0ddb429a6
Vaibhav Sathe <vaibhavsathe160@gmail.com>
2025-02-25 23:08:24 +0530

    endpoint

--- Backend/routes/projectRoute.js
+++ Backend/routes/projectRoute.js
@@ -18,17 +18,4 @@ router.post(
 
 router.get("/allprojects", authUser, getAllProjects);
 
-router.put(
-  "/addusers",
-  body("projectId").isString().withMessage("projects id must be a string"),
-  body("users")
-    .isArray({ min: 1 })
-    .withMessage("users must be an array of string")
-    .bail()
-    .custom((users) => users.every((user) => typeof user === "string"))
-    .withMessage("each user must be a string"),
-  authUser,
-  addUsersToProject
-);
-
 export default router;
