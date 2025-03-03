8a4a56ff9bd5fe25de4ce0ee617628788c79afee
Vaibhav Sathe <vaibhavsathe160@gmail.com>
2025-03-03 16:57:50 +0530

    Socket.io integrated successfully

--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,17 @@
 import "dotenv/config"
 import app from "./app.js";
 import http from "http"
+import { Server } from "socket.io";
 const PORT = process.env.PORT || 7889;
 
 const server = http.createServer(app)
 
-server.listen(PORT, () => console.log(`Server running at port: ${PORT}`))
+const io = new Server(server);
+
+io.on('connection', socket => {
+  console.log('a new client connected')
+  socket.on('event', data => { /* … */ });
+  socket.on('disconnect', () => { /* … */ });
+});
+
+server.listen(PORT, () => console.log(`Server running at port: http://localhost:${PORT}`))
