9c5c03f6176f1c90d8e24f7406366407dddc03d6
Vaibhav Sathe <vaibhavsathe160@gmail.com>
2025-03-09 01:35:03 +0530

    Project done

--- Backend/server.js
+++ Backend/server.js
@@ -57,7 +57,7 @@ io.on("connection", (socket) => {
     const message = data.message;
 
     const aiIsPresentInMessage = message.includes("@ai");
-
+    socket.broadcast.to(socket.roomId).emit("project-message", data);
     if (aiIsPresentInMessage) {
       const prompt = message.replace("@ai", "");
       const result = await generateResult(prompt);
@@ -72,8 +72,6 @@ io.on("connection", (socket) => {
 
       return;
     }
-
-    socket.broadcast.to(socket.roomId).emit("project-message", data);
   });
 
   socket.on("event", (data) => {
